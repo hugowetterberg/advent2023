af32514d45b0af20f51867701a0bd9bc4884eeef
Hugo Wetterberg <hugo@wetterberg.nu>
2023-12-06 06:40:21 +0100

    concurrency for day 5

--- day_05/part_two.go
+++ day_05/part_two.go
@@ -6,9 +6,16 @@ import (
 	"io"
 	"log/slog"
 	"math"
+	"runtime"
+	"sync"
 
 	"github.com/hugowetterberg/advent2023"
 )
+
+type seedRange struct {
+	Start  int
+	Length int
+}
 
 func PartTwo(logger *slog.Logger, r io.Reader) (*advent2023.IntegerResult, error) {
 	almanac, err := parseAlmanac(r)
@@ -20,23 +27,70 @@ func PartTwo(logger *slog.Logger, r io.Reader) (*advent2023.IntegerResult, error
 		return nil, errors.New("the almanac must contain an even number of seeds")
 	}
 
-	res := advent2023.IntegerResult{
-		Description: "Lowest location",
-		N:           math.MaxInt,
-	}
-
 	chain, err := almanac.MappingChain("location")
 	if err != nil {
 		return nil, fmt.Errorf("chain to location: %w", err)
 	}
 
-	for i := 0; i < len(almanac.Seeds); i += 2 {
-		start := almanac.Seeds[i]
-		len := almanac.Seeds[i+1]
+	rangeJobs := make(chan seedRange)
+	rangeResult := make(chan int)
 
-		for j := 0; j < len; j++ {
-			res.N = min(res.N, chain(start+j))
+	var wg sync.WaitGroup
+
+	go func() {
+		const batchSize = 1 << 20
+
+		for i := 0; i < len(almanac.Seeds); i += 2 {
+			fullRange := seedRange{
+				Start:  almanac.Seeds[i],
+				Length: almanac.Seeds[i+1],
+			}
+
+			for ss := 0; ss < fullRange.Length; ss += batchSize {
+				rangeJobs <- seedRange{
+					Start:  fullRange.Start + ss,
+					Length: min(batchSize, fullRange.Length-ss),
+				}
+			}
 		}
+
+		close(rangeJobs)
+	}()
+
+	for i := 0; i < runtime.NumCPU(); i++ {
+		wg.Add(1)
+
+		go func() {
+			for r := range rangeJobs {
+				if r.Length == 0 {
+					continue
+				}
+
+				n := math.MaxInt
+
+				for j := 0; j < r.Length; j++ {
+					n = min(n, chain(r.Start+j))
+				}
+
+				rangeResult <- n
+			}
+
+			wg.Done()
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(rangeResult)
+	}()
+
+	res := advent2023.IntegerResult{
+		Description: "Lowest location",
+		N:           math.MaxInt,
+	}
+
+	for n := range rangeResult {
+		res.N = min(res.N, n)
 	}
 
 	return &res, nil
